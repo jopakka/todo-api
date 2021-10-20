a4ea0cd65185a5653cba3b08813a04b7157315a9
jopakka <jopakkamies@gmail.com>
2021-10-20 17:12:32 +0300

    log for server

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,6 +1,7 @@
 const noteModel = require('../models/noteModel')
 
 const addNote = async (req, res) => {
+    console.log("ADD NOTE")
     const note = await noteModel.addNote(req.body.content)
     res.json(note)
 }
