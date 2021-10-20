13987fbeb5e7946dc2522d6647a3cb710df06cee
jopakka <jopakkamies@gmail.com>
2021-10-20 17:52:09 +0300

    getNote by id validator

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -19,7 +19,7 @@ router.route('/')
 
 router.route('/:id')
     .get(
-        param('id').isInt(),
+        param('id').trim().isInt().escape(),
         validate,
         notesController.getNote
     )
